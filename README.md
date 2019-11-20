7c3f800a7fcaf38a01d8143f4eabb7ecb70e219f
orpere <orlando.capoeiraraiz@gmail.com>
2019-11-20 15:18:51 +0100

    fix

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # tf-modules
 
 source = "git@github.com:orlando-pereira/tf-modules.git//webserver-cluster?ref=v0.0.1"
+“/webserver-cluster?ref=v0.0.2”
--- webserver-cluster/main.tf
+++ webserver-cluster/main.tf
@@ -117,27 +117,6 @@ resource "aws_security_group" "alb" {
   name = "${var.cluster_name}-alb"
 }
 
-module "webserver_cluster" {
-
-  # Since the terraform-up-and-running-code repo is open source, we're using an HTTPS URL here. If it was a private
-  # repo, we'd instead use an SSH URL (git@github.com:brikis98/terraform-up-and-running-code.git) to leverage SSH auth
-  #source = "github.com/brikis98/terraform-up-and-running-code//code/terraform/04-terraform-module/module-example/modules/services/webserver-cluster?ref=v0.1.0"
-  source = "git@github.com:orlando-pereira/tf-modules.git//webserver-cluster?ref=v0.0.2"
-
-  cluster_name           = var.cluster_name
-  db_remote_state_bucket = var.db_remote_state_bucket
-  db_remote_state_key    = var.db_remote_state_key
-
-  instance_type = "t2.micro"
-  min_size      = 2
-  max_size      = 2
-  
-  custom_tags = {
-    Owner      = "orlando"
-    DeployedBy = "terraform"
-  }
-}
-
 resource "aws_security_group_rule" "allow_http_inbound" {
   type              = "ingress"
   security_group_id = aws_security_group.alb.id
@@ -183,4 +162,3 @@ data "aws_vpc" "default" {
 data "aws_subnet_ids" "default" {
   vpc_id = data.aws_vpc.default.id
 }
-
